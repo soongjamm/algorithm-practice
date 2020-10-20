04f9293c76c18b25590e647060e08c3cf525d668
soongjamm <kimstz0303@gmail.com>
2020-10-20 09:00:12 +0000

    정수 삼각형 (수정)

--- dp/int_triangle.py
+++ dp/int_triangle.py
@@ -11,12 +11,15 @@ for _ in range(n):
 
 res = 0
 for i in range(1, n):
-  for j in range(i):
-    if j<=0:
+  for j in range(i+1):
+    if j==0:
       left = 0
     else:
-      left = dp[i-1][j]
-    right = max(dp[i-1][j], dp[i-1][j-1])
+      left = dp[i-1][j-1]
+    if j==i:
+      right = 0
+    else:
+      right = dp[i-1][j]
     dp[i][j] = dp[i][j] + max(left, right)
     res = max(res, dp[i][j])
 
