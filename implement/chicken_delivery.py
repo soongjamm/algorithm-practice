73d9adb63d925c6cb74234f406d0c3b61cb2048e
soongjamm <kimstz0303@gmail.com>
2020-11-02 11:23:55 +0000

    치킨 배달

--- implement/chicken_delivery.py
+++ implement/chicken_delivery.py
@@ -1,67 +1,39 @@
 # 치킨 배달
+# https://www.acmicpc.net/problem/15686
 
 from itertools import combinations
 
-# n:도시의 크기 n*n, m:폐업시키지 않을 치킨집 수
 n, m = map(int, input().split())
-chicken, house = [], []
+arr = []
+house = []
+chicken = []
 
-for r in range(n):
-  data = list(map(int, input().split()))
-  for c in range(n):
-    if data[c] == 1:
-      house.append((r,c)) # 일반 집
-    elif data[c] == 2:
-      chicken.append((r,c)) # 치킨집
+for i in range(n):
+  arr.append(list(map(int, input().split())))
 
-# 모든 치킨집 중에서 m개의 치킨집을 뽑는 조합 계산
-candidates = list(combinations(chicken, m))
+for i in range(n):
+  for j in range(n):
+    if arr[i][j] == 1:
+      house.append((i,j))
+    if arr[i][j] == 2:
+      chicken.append((i,j))
 
-# 치킨 거리의 합을 계산하는 함수
-def get_sum(candidate):
+# m개의 가능한 치킨집 조합 뽑아내기
+chickenList = list(combinations(chicken, m)) # m개의 좌표의 모든 경우의 수
+
+# chic = m개의 좌표 
+# 어느 chic 이 최선인지 계산해야 한다. 
+answer = int(1e9)
+for chic in chickenList: 
   result = 0
-  # 모든 집에 대하여
+  # 한 집에 대해 어떤 치킨집이 제일 가까운지 치킨 거리를 구한다.
+  # 치킨집은 m개의 치킨집에 한정됌
   for hx, hy in house:
-    # 가장 가까운 치킨집 찾기
-    temp = 1e9
-    for cx, cy in candidate:
-      temp = min(temp, abs(hx-cx) + abs(hy-cy))
-    # 가장 가까운 치킨집까지의 거리를 더하기
+    temp = int(1e9)
+    for cx, cy in chic:
+      temp = min(temp, abs(hx-cx) + abs(hy-cy)) # 
+    # 가장 가까운 치킨집까지의 거리를 더함
     result += temp
-  # 치킨 거리의 합 반환
-  return result
+  answer = min(answer, result)
 
-# 치킨 거리의 합의 최소를 찾아 출력
-result = 1e9
-for candidate in candidates:
-  result = min(result, get_sum(candidate))
-
-print(result)
-  
-
-
-# solution()
-
-# 답 : 5
-# 5 3
-# 0 0 1 0 0
-# 0 0 2 0 1
-# 0 1 2 0 0
-# 0 0 1 0 0
-# 0 0 0 0 2
-
-# 답 : 10
-# 5 2
-# 0 2 0 1 0
-# 1 0 1 0 0
-# 0 0 0 0 0
-# 2 0 0 1 1
-# 2 2 0 1 2
-
-# 답 : 11
-# 5 1
-# 1 2 0 0 0
-# 1 2 0 0 0
-# 1 2 0 0 0
-# 1 2 0 0 0
-# 1 2 0 0 0
+print(answer)
